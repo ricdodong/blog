af16f746a111e2f6460c0c2157f1d84eab7f4ede
Ric Dodong <ricalgen@gmail.com>
2026-08-02 22:11:39 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import font from '@astrojs/font'; // 1. Import font
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(), 
+    mdx(),
+    font({ // 2. Add font configuration
+      fonts: [
+        {
+          provider: 'google', // or 'fontsource'
+          name: 'Atkinson Hyperlegible',
+          cssVariable: '--font-atkinson', // <--- Must match the string passed to <Font />
+        },
+      ],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
